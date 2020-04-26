43ff435d991a32ed92e098b28c70306bc3f56b0b
王冠华 <ghwangd@163.com>
2020-04-26 10:55:07 +0800

    init project

--- weexkit.podspec
+++ weexkit.podspec
@@ -9,16 +9,16 @@
 Pod::Spec.new do |spec|
   spec.name         = "weexkit"
   spec.version      = "0.0.1"
-  spec.summary      = "A short description of weexkit."
-  spec.description  = "A longh description of weexkit. weex框架"
+  spec.summary      = "weex框架"
+  spec.description  = "weex 二次封装的框架"
   spec.homepage     = "https://github.com/wehuazi/weexkit"
   spec.license      = "MIT"
   spec.license      = { :type => "MIT", :file => "LICENSE" }
   spec.author             = { "王冠华" => "ghwangd@163.com" }
   spec.platform     = :ios
-  spec.source       = { :git => "https://github.com/wehuazi/weexkit", :tag => "#{spec.version}" }
-  spec.source_files  = "Classes", "Classes/**/*.{h,m}"
-  spec.public_header_files = "Classes/**/*.h"
+  spec.source       = { :git => "https://github.com/wehuazi/weexkit.git", :tag => "#{spec.version}" }
+  spec.source_files  = "weexkit", "weexkit/**/*.{h,m}"
+  spec.public_header_files = "weexkit/**/*.h"
   spec.requires_arc = true
   # spec.ios.vendored_libraries = "Class"
 
--- weexkit/AppDelegate.m
+++ weexkit/AppDelegate.m
@@ -1,12 +1,13 @@
 //
 //  AppDelegate.m
-//  weexkit
+//  ZMTProject
 //
-//  Created by 王冠华 on 2020/4/26.
+//  Created by 王冠华 on 2020/4/17.
 //  Copyright © 2020 ZMT. All rights reserved.
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,25 +18,63 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[ViewController alloc] init];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    if (![deviceToken isKindOfClass:[NSData class]]) return;
+    const unsigned *tokenBytes = (const unsigned *)[deviceToken bytes];
+    NSString *hexToken = [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",
+                          ntohl(tokenBytes[0]), ntohl(tokenBytes[1]), ntohl(tokenBytes[2]),
+                          ntohl(tokenBytes[3]), ntohl(tokenBytes[4]), ntohl(tokenBytes[5]),
+                          ntohl(tokenBytes[6]), ntohl(tokenBytes[7])];
+    NSLog(@"deviceToken:%@",hexToken);
+    /// Required - 注册 DeviceToken
+    //    [JPUSHService registerDeviceToken:deviceToken];
+}
 
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    __block UIBackgroundTaskIdentifier bgTask = [[UIApplication sharedApplication] beginBackgroundTaskWithName:@"bg_task" expirationHandler:^{
+        [[UIApplication sharedApplication] endBackgroundTask:bgTask];
+        bgTask =UIBackgroundTaskInvalid;
+    }];
 }
 
 
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    //    [self setBadge:0 application:application];
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+// IOS9.0 系统新的处理openURL 的API
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<NSString *,id> *)options {
+    return YES;
+}
 
 @end
